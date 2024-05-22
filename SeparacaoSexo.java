26439b95056d324bf4f96451baa068b66890d327
JairoTJ <Jairotjunior@hotmail.com>
2024-05-22 16:56:04 -0300

    Inicio do exercício 'Coleções parte 2'

---
+++ SeparacaoSexo.java
@@ -0,0 +1,14 @@
+package Java.Colecoes_Parte1;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SeparacaoSexo {
+  public static void main(String args[]){
+    SeparacaoSexo ss = new SeparacaoSexo();
+    ss.cadastro();
+  }
+
+  public void cadastro(){
+
+}
